336c206566dd2277def7782c549dbb130b91251c
gvsiva2008 <gvsiva2008gmail.com>
2022-03-15 07:45:11 +0530

    files added

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any 
+    stages {
+        stage ('git source') {
+            steps {
+                script {
+                    git clone https://github.com/DEVOPS-7am/simple_freestyle.git
+                }
+                 
+            }
+        }
+        stage ('build') {
+            steps {
+                script {
+                    mvn clean install
+                }
+            }
+        }
+    }
+}
